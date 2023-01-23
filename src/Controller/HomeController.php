55b696c76d2499fe1df9e6f438712c230de2dc18
PickleBoxer <matic.vertacnik@icloud.com>
2023-01-23 13:40:28 +0100

    Request data through a Request object

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * HomeController
+ */
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+
+    /**
+     * index
+     *
+     * @param  mixed $request
+     * @return Response
+     */
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('Hello %s!', $name);
+        }
+
+        dump($request); //Debugging Variables
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'greet' => $greet,
         ]);
     }
 }
