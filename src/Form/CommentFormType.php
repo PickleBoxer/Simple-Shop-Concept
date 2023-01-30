8d76e72bd7b1d9244a9847832b500437fa160095
PickleBoxer <matic.vertacnik@icloud.com>
2023-01-30 14:11:38 +0100

    Customizing a Form Type Comment

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -6,18 +6,31 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Image;
 
+// The App\Form\CommentFormType class defines a form for the App\Entity\Comment
 class CommentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
+            ->add('author', null, ['label' => 'Your name',])
             ->add('text')
-            ->add('email')
-            ->add('createdAt')
-            ->add('photoFilename')
-            ->add('product')
+            ->add('email', EmailType::class)
+            // Entity only needs to save the photo filename, but the form has to deal with the file upload itself
+            // To handle this case, we have added a field called photo as un-mapped field
+            ->add('photo', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new Image(['maxSize' => '1024k'])
+                ],
+            ])
+            // added a submit button
+            ->add('submit', SubmitType::class)
         ;
     }
 
