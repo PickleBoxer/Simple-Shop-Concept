88b03a46032448a6b38f3aefc6ee3c43776ce099
PickleBoxer <matic.vertacnik@icloud.com>
2023-02-15 11:52:04 +0100

    Creating the Factory for Order to Control the Creation of Objects

---
+++ src/Factory/OrderFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Order;
+use App\Entity\OrderItem;
+use App\Entity\Product;
+
+// The OrderFactory factory will help us to create Order and OrderItem entities with default data. 
+// It also allows you to change the Order entity easily.
+
+/**
+ * Class OrderFactory.
+ */
+class OrderFactory
+{
+    /**
+     * Creates an order.
+     */
+    public function create(): Order
+    {
+        $order = new Order();
+        $order
+            ->setStatus(Order::STATUS_CART)
+            ->setCreatedAt(new \DateTime())
+            ->setUpdatedAt(new \DateTime());
+
+        return $order;
+    }
+
+    /**
+     * Creates an item for a product.
+     */
+    public function createItem(Product $product): OrderItem
+    {
+        $item = new OrderItem();
+        $item->setProduct($product);
+        $item->setQuantity(1);
+
+        return $item;
+    }
+}
