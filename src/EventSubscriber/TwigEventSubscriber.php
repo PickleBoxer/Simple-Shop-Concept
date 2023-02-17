2dc3d5335fe527d8f5b69e09baf4691de9c090af
PickleBoxer <matic.vertacnik@icloud.com>
2023-02-17 12:39:45 +0100

    Add Cart Items Count Indicator

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -3,6 +3,7 @@
 namespace App\EventSubscriber;
 
 use App\Repository\CategoryRepository;
+use App\Manager\CartManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -12,13 +13,15 @@ class TwigEventSubscriber implements EventSubscriberInterface
 {
     private $twig;
     private $categoryRepository;
+    private $cartManager;
 
     // Inject dependencies via constructor
     // To do this you need to add an argument to the constructor signature to accept the dependency
-    public function __construct(Environment $twig, CategoryRepository $categoryRepository)
+    public function __construct(Environment $twig, CategoryRepository $categoryRepository, CartManager $cartManager)
     {
         $this->twig = $twig;
         $this->categoryRepository = $categoryRepository;
+        $this->cartManager = $cartManager;
     }
 
     // Event which is dispatched just before the controller is called
@@ -27,6 +30,7 @@ class TwigEventSubscriber implements EventSubscriberInterface
     {
         // add as many controllers as you want: the categories variable will always be available in Twig
         $this->twig->addGlobal('categories', $this->categoryRepository->findAll());
+        $this->twig->addGlobal('currentCart', $this->cartManager->getCurrentCart()->getItems());
     }
 
     public static function getSubscribedEvents(): array
